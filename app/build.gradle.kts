3f32ea8ba329229bdf426ea6bc9147b069da0c56
ultfone <ojasdwivedi313@gmail.com>
2025-04-02 22:32:15 +0530

    Meaninglessness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
